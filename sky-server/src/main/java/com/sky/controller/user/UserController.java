51b3c7de81abfa03ede94946ee2cdc38aafac6d5
Fei Ding <dingfei.sgad@gmail.com>
2025-05-05 11:37:17 +0800

    Add User log in and log out function

--- sky-server/src/main/java/com/sky/controller/user/UserController.java
+++ sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -3,16 +3,21 @@ package com.sky.controller.user;
 import com.sky.dto.UserLoginDTO;
 import com.sky.result.Result;
 import com.sky.vo.UserLoginVO;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user/user")
 public class UserController {
-    @GetMapping("/login")
+    @PostMapping("/login")
     Result<UserLoginVO> login(UserLoginDTO userLoginDTO) {
-        //UserLoginVO userLoginVO =
-        return null;
+        UserLoginVO userLoginVO = new UserLoginVO();
+        return Result.success(userLoginVO);
+    }
+
+    @PostMapping("/logout")
+    Result<Void> logout() {
+        return Result.success();
     }
 }
