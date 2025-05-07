cc704a888a7e71cb440200b9271af9c222051bd1
Fei Ding <dingfei.sgad@gmail.com>
2025-05-07 23:21:26 +0800

    Strengthen CommonController

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -10,17 +10,36 @@ import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
+/**
+ * Common request controller (Deal with file uploading)
+ */
 @RestController
 @RequestMapping("/admin/common")
 public class CommonController {
+    /**
+     * Upload the file (image)
+     *
+     * @param file The uploaded file
+     * @return Operation results with absolute path of the file stored
+     * @throws IOException if file transfer fails
+     * @throws IllegalArgumentException if the file is null or does not have the extension
+     */
     @PostMapping("/upload")
-    public Result<String> upload(MultipartFile file) throws IOException {
+    public Result<String> upload(MultipartFile file) throws IOException, IllegalArgumentException {
+        String filePathStored = "D:/Software Development/Sky Take out/sky-take-out-back-end/repository";
+
         String originalFilename = file.getOriginalFilename();
-        assert originalFilename != null;
+        if (originalFilename == null) {
+            throw new IllegalArgumentException("Filename cannot be null");
+        }
         int lastIndex = originalFilename.lastIndexOf(".");
+        if (lastIndex == -1) {
+            throw new IllegalArgumentException("The filename should have an extension");
+        }
+
         String extension = originalFilename.substring(lastIndex);
         String newFilename = UUID.randomUUID().toString() + extension;
-        File storageFile = new File("D:/Software Development/Sky Take out/sky-take-out-back-end/repository", newFilename);
+        File storageFile = new File(filePathStored, newFilename);
         file.transferTo(storageFile);
         return Result.success(storageFile.getAbsolutePath());
     }
